cb3c4b0dbd58d81a9e680ea59e85a003105b97aa
Katuniakz <katuniakz@gmail.com>
2024-07-26 16:52:33 +0700

    memulai checkpoint 1

---
+++ krustykrab.js
@@ -0,0 +1,34 @@
+ const krustyKrab = {
+    name: "Krusty Krab",
+    specialRecipe: {
+        name: "Krabby Patty",
+        ingredients: ["bread", "tomatoes", "beef", "plankton :D"],
+        isDelicious: function (purchasesPerday){
+            return purchasesPerday > 500;
+        }
+    },
+        getEmployeeOfTheMOnth: function () {
+            return {
+                name: "Spongebob",
+                age: 12;
+        };
+    }
+ };
+
+ console.log (is Krabby Patty delicious? $ {isKrabbyPattyDelicious});
+
+var restaurantName = 'Krusty Krab';
+var specialRecipe = 'Krabby Patty';
+var specialReceipeInggredients = ['Bread', 'Tomatoes','Beef', 'Plankton'];
+var purchase = 600;
+var isDelicious = purchase >= 500;
+
+function getEmployeeOfTheMOnth () {
+    var employee = {
+        name: 'Spongebob',
+        age: 12,
+    }
+    return employee;
+}
+
+console.log (getEmployeeOfTheMOnth());
